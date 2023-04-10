e1a77b3cf919b61e91bad6af62c7584c43c41d53
Vegatality <123563774+Vegatality@users.noreply.github.com>
2023-04-10 10:22:30 +0900

    [level 1] Title: 모의고사, Time: 2.78 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.js
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -1,72 +1,23 @@
 function solution(answers) {
-    let first = 1
-    let second = [2, 1, 2, 3, 2, 4, 2, 5]
-    let third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    let thirdFlag = 0
-    let secondFlag = 0
+    // 각자만의 규칙
+    let st1 = [1, 2, 3, 4, 5]
+    let st2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    let st3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
 
-    // 첫째 맞은 개수
-    let firstCount = answers.filter((v) => {
-        if (first > 5) {
-            first = 1
-            return v === first++
-        } else {
-            return v === first++
-        }
-    }).length
+    // 인덱스 % 배열길이(5) = 나머지  0,1,2,3,4 % 배열길이  //    5%5 = [0]
 
-    // 두 번째 맞은 개수
-    let secondCount = answers.filter((ele, idx) => {
-        if (secondFlag > 7) {
-            secondFlag = 0
-            return ele === second[secondFlag++]
-        } else {
-            return ele === second[secondFlag++]
-        }
-    }).length
+    // 각자 규칙에 맞춰 맞춘 개수
+    let st1Count = answers.filter((ele,idx)=> ele === st1[idx%st1.length]).length
+    let st2Count = answers.filter((ele,idx)=> ele === st2[idx%st2.length]).length
+    let st3Count = answers.filter((ele,idx)=> ele === st3[idx%st3.length]).length
 
-    // 셋째 맞은 개수
-    let thirdCount = answers.filter((ele, idx) => {
-        if (thirdFlag > 9) {
-            thirdFlag = 0
-            return ele === third[thirdFlag++]
-        } else {
-            return ele === third[thirdFlag++]
-        }
-    }).length
-
-    // 지금 일단 개수는 구했음. 그 개수를 누가 가지고 있느냐 이거지.
-
-    let final = [firstCount, secondCount, thirdCount]
-    let max = Math.max(...[firstCount, secondCount, thirdCount])
     let Box = []
-    for(let i=0; i < final.length; i++){
-        if(final[i] === max){
-            Box.push(i+1)
-        }
-    }
+    let mx = Math.max(st1Count, st2Count, st3Count)
+
+    // 순서 지정해서.
+    if(st1Count === mx) Box.push(1)
+    if(st2Count === mx) Box.push(2)
+    if(st3Count === mx) Box.push(3)
+
     return Box
 }
-
-
-
-
-// function solution(answers) {
-//     let answer = []
-//     var a1 = [1, 2, 3, 4, 5];
-//     var a2 = [2, 1, 2, 3, 2, 4, 2, 5]
-//     var a3 = [ 3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-
-//     let a1c = answers.filter((ele, idx) => ele === a1[idx%a1.length]).length
-//     let a2c = answers.filter((ele, idx) => ele === a2[idx%a2.length]).length
-//     let a3c = answers.filter((ele, idx) => ele === a3[idx%a3.length]).length
-
-//     let max = Math.max(a1c, a2c, a3c)
-
-//     if(a1c === max) answer.push(1) 
-//     if(a2c === max) answer.push(2) 
-//     if(a3c === max) answer.push(3) 
-
-//     return answer
-// }
-// console.log(solution([1,3,2,4,2]))

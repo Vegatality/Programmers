8fff629672af2abc61a3504e2310239fb3bcf2a7
Vegatality <123563774+Vegatality@users.noreply.github.com>
2023-04-10 15:58:31 +0900

    [level 1] Title: 약수의 합, Time: 0.18 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -0,0 +1,12 @@
+
+function solution(n) {
+    let box = []
+    for (let i= 1; i <= n; i++){
+        if (n % i === 0 && n!==0) {
+            box.push(i)
+        }
+
+    }
+    
+       return box.reduce((a,b)=>a+b, 0)
+}

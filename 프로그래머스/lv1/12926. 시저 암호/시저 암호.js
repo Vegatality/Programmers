6cf9e4413cb79d2a8189f76a8577fb28fe0cf425
Vegatality <123563774+Vegatality@users.noreply.github.com>
2023-04-10 11:43:54 +0900

    [level 1] Title: 시저 암호, Time: 2.77 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
@@ -1,17 +1,17 @@
 function solution(s, n) {
-    let dict = "abcdefghijklmnopqrstuvwxyz"
-    let dictBig = dict.toUpperCase()
-    // console.log(dict[0]) // a
-    // let dictBox = dict.split("").length
-    // console.log(dictBox) // 26 // 마지막 z의 인덱스는 25
-    let sBox = s.split("")
+    // let dict = /[a-z]/g        /
+    let dict = "abcdefghijklmnopqrstuvwxyz" // "abcdefghijklmnopqrstuvwxyz"
+    let bigDict = dict.toUpperCase()     // "ABCDE....................Z"
+    console.log(bigDict)
+    let sBox = s.split("")  
     return sBox.map((ele,idx)=> {
         if(ele === " "){
-            return " "
-        } else if(ele > "Z"){
-            return dict[(dict.indexOf(ele)+n) % 26]
-        } else {
-            return dictBig[(dictBig.indexOf(ele)+n) % 26]
+            return " " // " "
+        } else if(ele > "Z" ){ 
+            return dict[(dict.indexOf(ele) + n) % 26] 
+        } else { 
+            return bigDict[(bigDict.indexOf(ele) + n) % 26] 
         }
     }).join("")
+
 }

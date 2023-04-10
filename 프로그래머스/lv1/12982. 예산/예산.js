6a4184d6e28abf162ddc72a1f561e47e9ad7350a
Vegatality <123563774+Vegatality@users.noreply.github.com>
2023-04-10 22:27:28 +0900

    [level 1] Title: 예산, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.js
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -1,14 +1,13 @@
-function solution (d, budget) {
-    let newD = d.sort((a,b)=> a-b)
-    let count = 0;
-    for(let i = 0; i < newD.length; i++){
-        if(budget >= newD[i]){
-            budget -= newD[i]
-            count ++
-        } else {
-            break
-        }
+function solution(d, budget) {
+  let box = d.sort((a, b) => a - b); // 오름차순으로 정렬
+  let count = 0;
+  for (let i = 0; i < box.length; i++) {
+    if (budget >= box[i]) {
+      budget -= box[i];
+      count++;
+    } else { // budget < box[i]
+      break;
     }
-    return count
+  }
+  return count;
 }
-

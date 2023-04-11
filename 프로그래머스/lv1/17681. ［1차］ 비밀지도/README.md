bdb6129d91738b38c082c596a6efb3ab42512dbe
Vegatality <123563774+Vegatality@users.noreply.github.com>
2023-04-11 19:04:29 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/README.md
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/README.md
@@ -1,10 +1,10 @@
 # [level 1] [1차] 비밀지도 - 17681 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17681) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17681?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.08 ms
+메모리: 33.5 MB, 시간: 0.07 ms
 
 ### 구분
 
--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,11 +1,31 @@
+// function solution(n, arr1, arr2) {
+//     let result = []
+//     for (let i = 0; i < n; i++) {
+//         let queue1 = (parseInt(arr1[i].toString(2)) + parseInt(arr2[i].toString(2))).toString()
+//         console.log(queue1)
+//         if(arr1.length < n){
+//             // .replace(/^/, "0")
+//             queue1 = queue1.replace(/^ /, "0") // "01001"
+//             console.log(queue1)
+//         }
+//             result.push(queue1.replaceAll("1","#").replaceAll("2","#").replaceAll("0", " ")) // ["#####","# # #", "### #", "# ##", "#####"]
+//             console.log(result)
+//         }
+//     }
+//     return result
+// }
+
+
 function solution(n, arr1, arr2) {
     let result = []
     for (let i = 0; i < n; i++) {
         let queue1 = (parseInt(arr1[i].toString(2)) + parseInt(arr2[i].toString(2))).toString()
-        if(queue1.length < n){
-            queue1 = "0".repeat(n - queue1.length) + queue1
+        while(queue1.length < n){
+            // .replace(/^/, "0")
+            queue1 = queue1.replace(/^/, "0") // "01001"
+            // console.log(queue1)
         }
-        result.push(queue1.replaceAll("1","#").replaceAll("2","#").replaceAll("0", " "))
+        result.push(queue1.replaceAll("1","#").replaceAll("2","#").replaceAll("0", " ")) // ["#####","# # #", "### #", "# ##", "#####"]
     }
     return result
 }

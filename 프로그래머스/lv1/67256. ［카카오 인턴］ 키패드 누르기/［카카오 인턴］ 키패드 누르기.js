ea0131c274c210ea3c9e1dc6a728e9e62676ae60
Vegatality <123563774+Vegatality@users.noreply.github.com>
2023-04-12 22:58:48 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 1.39 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
@@ -18,20 +18,22 @@ function solution(numbers, hand) {
     for (let i of numbers) {
         if (i === 1 || i === 4 || i === 7) {
             answer += 'L';
-            left = i; // 1  // 4
+            left = i; // 1  // 4 // 5 // 8 // 1 // 4
         }else if (i === 3 || i === 6 || i === 9) {
             answer += 'R'; 
-            right = i - 2;  // 3-2 = 1
+            right = i;  // 3-2 = 1   3// 2 // 9 right의 현 위치에서 2를 까주는 이유는 ?
+            // right = i - 2;  // 3-2 = 1   3// 2 // 9 right의 현 위치에서 2를 까주는 이유는 ?
         }else {
             //                     
             // [[1, 2, 3],
             // [4, 5, 6],
             // [7, 8, 9],
             // [*, 0, #]]
+            // 숫자로 이동을 한게 아니라 숫자를 비교해서 인덱스 차이로 거리를 구한 거임.
             // 숫자 빼기 숫자로 구현한게 아니라 배열의 인덱스 빼기 인덱스로 구현을 한 것.!!!
-            let r = findKey(right); // "LRL"  findkey(1)  => [0, 0] 이게 결국 1을 찾아온는거  // 움직이기 전 r의 위치?  
-            let l = findKey(left); // findkey(4)    => [1, 0]   [y, x]      // 움직이기 전 l의 위치?
-            let middle = findKey(i);  // findkey(5) => [1,1]         // 지금 목표로 하고 있는 중앙의! 위치
+            let r = findKey(right); // "LRL"  findkey(1)  => [0, 0] 이게 결국 1을 찾아온는거  // 움직이기 전 r의 위치?  [0,2]
+            let l = findKey(left); // findkey(4)    => [1, 0]   [y, x]      // 움직이기 전 l의 위치?  [2,2]
+            let middle = findKey(i);  // findkey(5) => [1,1]         // 지금 목표로 하고 있는 중앙의! 위치 [0, 1]
             // 손위치와 눌러야할 키패드 거리 구하기
             let rr = Math.abs(r[0] - middle[0]) + Math.abs(r[1] - middle[1]);   // (0 - 1) + (0 - 1) = 2  ===>>>  [ 위아래좌표  ,   좌우좌표 ]
             let ll = Math.abs(l[0] - middle[0]) + Math.abs(l[1] - middle[1]);   // (1 - 1) + (0 - 1) = 1

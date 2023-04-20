183c99ebae3390d449d345252e5d57b9ec8d9a76
Vegatality <123563774+Vegatality@users.noreply.github.com>
2023-04-20 21:53:32 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.41 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,28 +1,45 @@
 
-function solution(left, right) {
-    let box = []   // left 24   right 27
-    for (let i = left; i < right + 1; i++) {  // 24 25 26 27
-        box.push(i)
-    }
-    // [24 25 26 27]
-    let countBox = []
-    for(let i = 0; i < box.length; i++){
-        let count = 0  // 여기 count는 24 => 25 넘어갈 때 초기화 됨.
-        for(let j = 1; j < box[i] + 1; j++){
-            if(box[i] % j === 0){
-                count++
-                 // 24 만의 카운트를 카운트박스에 담음.
-            }
+// function solution(left, right) {
+//     let box = []   // left 24   right 27
+//     for (let i = left; i < right + 1; i++) {  // 24 25 26 27
+//         box.push(i)
+//     }
+//     // [24 25 26 27]
+//     let countBox = []
+//     for(let i = 0; i < box.length; i++){
+//         let count = 0  // 여기 count는 24 => 25 넘어갈 때 초기화 됨.
+//         for(let j = 1; j < box[i] + 1; j++){
+//             if(box[i] % j === 0){
+//                 count++
+//                  // 24 만의 카운트를 카운트박스에 담음.
+//             }
 
-        } // 반복문이 끝나고 count가 8이 되서 나옴. // count 3이 되서 나옴
-        countBox.push(count) // [8, 3, 4, 4]
-    }
+//         } // 반복문이 끝나고 count가 8이 되서 나옴. // count 3이 되서 나옴
+//         countBox.push(count) // [8, 3, 4, 4]
+//     }
+//     let result = 0;
+//     for(let i=0; i<countBox.length; i++){
+//         if(countBox[i]%2===0){
+//             result += box[i]
+//         } else {
+//             result -= box[i]
+//         }
+//     }
+//     return result
+// }
+function solution (left, right){
     let result = 0;
-    for(let i=0; i<countBox.length; i++){
-        if(countBox[i]%2===0){
-            result += box[i]
+    for(let i = left; i < right + 1; i++){
+        let count = 0;
+        for(let j = 1; j < i + 1; j++){
+            if(i % j === 0) {
+                count += 1
+            }
+        }
+        if(count % 2 === 0){
+            result += i
         } else {
-            result -= box[i]
+            result -= i
         }
     }
     return result

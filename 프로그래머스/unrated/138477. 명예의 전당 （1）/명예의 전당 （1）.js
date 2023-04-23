153207f1d198730b818ef6185ad2028bd1dc7edb
Vegatality <123563774+Vegatality@users.noreply.github.com>
2023-04-23 22:51:44 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,22 +1,16 @@
-function solution(k, score) {
-    const honor = []; // [10, 100, 20]
-    const result = [];
-    for (let i = 0; i < score.length; i++) {
-        // 명예의 전당 기본 셋팅
-        // k보다 작은 경우는 (아직 k일 만큼 안지나서 점수가 k보다 덜 쌓여있는 경우)
-        if (i < k) {
-            honor.push(score[i]);
+function solution(k, score){
+    let honor = []
+    let result = []
+    for(let i=0; i<score.length; i++){
+        if(i < k){
+            honor.push(score[i])
         }
-        // k보다 같거나 커지는 순간(가장 작은애랑 )
-        // 만약 k가 3이라고 치면  // i가 4가 되는 순간
-        // 이번 점수가 명예의 전당 최하위 점수보다 큰 경우
-        if (score[i] > Math.min(...honor)) {
-            honor.pop();
-            honor.push(score[i]);
-            honor.sort((a, b) => b - a);
+        if(score[i] > Math.min(...honor)){
+            honor.pop()
+            honor.push(score[i])
+            honor.sort((a,b)=>b-a)
         }
-        // 결괏값에 명예의 전당 중 최하위 점수 입력
-        result.push(honor[honor.length - 1]);
+        result.push(honor.at(-1))
     }
-    return result;
+    return result
 }
